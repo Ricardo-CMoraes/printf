d9472d2efada09232c0bd6a8998e12fd0d7f3654
Ricardo <ric.costamoraes@gmail.com>
2025-08-09 16:33:07 -0300

    ajuste no .h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rida-cos <rida-cos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 23:17:39 by rdcm              #+#    #+#             */
-/*   Updated: 2025/08/09 14:45:07 by rida-cos         ###   ########.fr       */
+/*   Updated: 2025/08/09 16:30:55 by rida-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int					ft_printf(const char *text, ...);
 int					ft_putstr(char *s, int fd);
 int					ft_putnbr(int n, int fd);
 int					ft_putptr(void *addr);
-unsigned long long	n_digits(unsigned long long n, size_t base);
 int					ft_puthex(unsigned int nb, char type);
 unsigned int		ft_putnbr_unsigned(unsigned int n, int fd);
+unsigned long long	n_digits(unsigned long long n, size_t base);
 
 #endif
